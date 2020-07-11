253c4301a586cde29f3ee4b4b8d9a20ead310bd0
kierancoz <kierancoz.w@gmail.com>
2020-07-11 15:18:09 -0500

    working state

---
+++ poker/build.bat
@@ -0,0 +1,4 @@
+set ahk_compiler="Z:\Game Storage\code\autohotkey\Compiler\Ahk2Exe.exe"
+set py_path=
+
+%ahk_compiler% /in getGameInfo.ahk /out ..\pokerstar-control\bin\getGameInfo.exe
--- poker/image_converter.py
+++
@@ -1,9 +0,0 @@
-
-
-class ImageConverter(object):
-    def __init__(self, work_dir):
-        self.work_dir_ = work_dir
-
-    @property
-    def temp(self):
-        return self.work_dir_
